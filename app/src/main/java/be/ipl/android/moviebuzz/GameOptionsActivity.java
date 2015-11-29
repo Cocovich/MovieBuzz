a683becf1b9bfd27785e3ce543dcb9f4e92d17cb
actaruss <arnaudkandala@gmail.com>
2015-11-29 03:56:55 +0100

    BLUETOOTH4

--- app/src/main/java/be/ipl/android/moviebuzz/GameOptionsActivity.java
+++ app/src/main/java/be/ipl/android/moviebuzz/GameOptionsActivity.java
@@ -1,6 +1,8 @@
 package be.ipl.android.moviebuzz;
 
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
@@ -22,6 +24,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -42,21 +46,38 @@ public class GameOptionsActivity extends AppCompatActivity {
     private int min, max, step;
 
     // pour permission bluetooth
+    ArrayList<BluetoothDevice>deviceList;
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
     private Set<BluetoothDevice> devices;
+    private ProgressDialog mProgressDlg;
     private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
         //des quon detecte un divice il fera cette methode
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Toast.makeText(getApplicationContext(), "New Device = " + device.getName(), Toast.LENGTH_SHORT).show();
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                deviceList = new ArrayList<BluetoothDevice>();
+                mProgressDlg.show();
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                mProgressDlg.dismiss();
+
+                Intent newIntent = new Intent(getApplicationContext(), DeviceListActivity.class);
+
+                newIntent.putParcelableArrayListExtra("device.list", deviceList);
+
+                startActivity(newIntent);
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                deviceList.add(device);
+
+                toaster("Found device " + device.getName());
             }
         }
     };
-    private IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-    Intent registerReceiver = null;//a destroy a la fin
+    private IntentFilter filter ;
+
+    private Intent registerReceiver = null;//a destroy a la fin
     private static final UUID MY_UUID_INSECURE = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
     private static final String NAME_INSECURE = "BluetoothInsecure";
     //on linstancie slmnt si clic sur trouverchallenger
@@ -68,9 +89,36 @@ public class GameOptionsActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
         registerReceiver = registerReceiver(bluetoothReceiver, filter);
+        mProgressDlg = new ProgressDialog(this);
+        mProgressDlg.setMessage("Scanning...");
+        mProgressDlg.setCancelable(false);
+        mProgressDlg.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                bluetoothAdapter.cancelDiscovery();
+            }
+        });
 
         setContentView(R.layout.activity_game_options);
+    }
+
+    @Override
+    public void onPause() {
+        if (this.bluetoothAdapter != null) {
+            if (this.bluetoothAdapter.isDiscovering()) {
+                this.bluetoothAdapter.cancelDiscovery();
+            }
+        }
+
+        super.onPause();
     }
 
     @Override
@@ -125,7 +173,7 @@ public class GameOptionsActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 gameIntent.putExtra(key, (seekBar.getProgress() + min / step) * step);
                 findChallenger();
-                startActivity(gameIntent);
+           //     startActivity(gameIntent);
             }
         });
 
@@ -251,8 +299,7 @@ public class GameOptionsActivity extends AppCompatActivity {
         } else {
             message = "Ok vous vous posssedez le bluetooth";
         }
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-
+        toaster(message);
         //si son bluetooth n'est pas activer je demande permission de lactiver
         if (!bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -261,6 +308,9 @@ public class GameOptionsActivity extends AppCompatActivity {
 
     }
 
+    public void toaster(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+    }
 
     public void findChallenger() {
         //retest bluetooth
@@ -268,15 +318,18 @@ public class GameOptionsActivity extends AppCompatActivity {
             verificationBluetooth();
         // affichage de tt peripherik bluetooth kon connait... DEBBUG
         devices = bluetoothAdapter.getBondedDevices();
-        for (BluetoothDevice blueDevice : devices) {
-            Toast.makeText(getApplicationContext(), "Device = " + blueDevice.getName(), Toast.LENGTH_SHORT).show();
-        }
+        if (devices == null || devices.size() == 0)
+            toaster("No Paired Devices Found");
+        else
+            for (BluetoothDevice blueDevice : devices) {
+                toaster("Device = " + blueDevice.getName());
+            }
         //recherche des peripherique inconnu
         if (bluetoothAdapter.isDiscovering()) {
             bluetoothAdapter.cancelDiscovery();
         }
         bluetoothAdapter.startDiscovery();
-       //une fois que jai choisi le bon je dois initialiser mon client et me connecter
+        //une fois que jai choisi le bon je dois initialiser mon client et me connecter
         //COCO
         // clientBluetooth=new ClientBluetooth();
 
@@ -292,31 +345,27 @@ public class GameOptionsActivity extends AppCompatActivity {
             return;
         // si il a dit oui
         if (resultCode == RESULT_OK) {
-            toast = Toast.makeText(getApplicationContext(), "bluetooth ACTIVER", Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.CENTER, 0, 0);
-            toast.show();
+            toaster("bluetooth ACTIVER");
             rendreVisibleEnBluetooth();
-
         } else {
-            toast = Toast.makeText(getApplicationContext(), "bluetooth NON activer", Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.CENTER, 0, 0);
-            toast.show();
+            toaster("bluetooth NON activer");
         }
     }
-    public  void rendreVisibleEnBluetooth(){
+
+    public void rendreVisibleEnBluetooth() {
         if (bluetoothAdapter.isEnabled()) {
             //une fois le bluetooth activer alor on peut demarer le serveur
             // if (!serveurBluetooth.getState().equals(BluetoothAdapter.STATE_OFF) && !serveurBluetooth.getState().equals(BluetoothAdapter.STATE_TURNING_OFF))
             //   serveurBluetooth.start();
             //rendre mon peripherique visible
-            Toast.makeText(getApplicationContext(), "rendre visible", Toast.LENGTH_SHORT).show();
+            toaster("rendre visible");
             Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
             discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
             startActivity(discoverableIntent);
             // affichage de tt peripherik bluetooth kon connait... DEBBUG
             devices = bluetoothAdapter.getBondedDevices();
             for (BluetoothDevice blueDevice : devices) {
-                Toast.makeText(getApplicationContext(), "Device = " + blueDevice.getName(), Toast.LENGTH_SHORT).show();
+                toaster("Device = " + blueDevice.getName());
             }
 
         }
